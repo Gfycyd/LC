7d3f569ccb750c5d466eb10dd1b8a4ea2453c1e9
Gfycyd <31628809+Gfycyd@users.noreply.github.com>
2021-03-07 17:58:49 +0300

    Add files via upload

---
+++ LC/LC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LC
+//
+//  Created by Екатерина Узбекова on 17.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Construct Love")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LC/LC/LCApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCApp.swift
+//  LC
+//
+//  Created by Екатерина Узбекова on 17.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct LCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
